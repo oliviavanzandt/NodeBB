cbcf56f8f7af1090aa23b8504128b8ae0d1a3ba5
oliviavz <vzolivia@gmail.com>
2023-09-06 00:06:36 -0400

    currently passing 8 tests and failing one, have 19 errors in the linter, have typsecript for everything but failing lots of types any that i need to fix

--- src/messaging/notifications.js
+++ src/messaging/notifications.js
@@ -31,79 +31,92 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-Object.defineProperty(exports, "__esModule", { value: true });
 const winston = __importStar(require("winston"));
 const user = __importStar(require("../user"));
 const notifications = __importStar(require("../notifications"));
 const sockets = __importStar(require("../socket.io"));
 const plugins = __importStar(require("../plugins"));
 const meta = __importStar(require("../meta"));
-'use strict';
-module.exports = function (Messaging) {
-    Messaging.notifyQueue = {}; // Only used to notify a user of a new chat message, see Messaging.notifyUser
-    Messaging.notifyUsersInRoom = (fromUid, roomId, messageObj) => __awaiter(this, void 0, void 0, function* () {
-        let uids = yield Messaging.getUidsInRoom(roomId, 0, -1);
-        uids = yield user.blocks.filterUids(fromUid, uids);
-        let data = {
-            roomId: roomId,
-            fromUid: fromUid,
-            message: messageObj,
-            uids: uids,
-        };
-        data = yield plugins.hooks.fire('filter:messaging.notify', data);
-        if (!data || !data.uids || !data.uids.length) {
-            return;
-        }
-        uids = data.uids;
-        uids.forEach((uid) => {
-            data.self = parseInt(uid, 10) === parseInt(fromUid, 10) ? 1 : 0;
-            Messaging.pushUnreadCount(uid);
-            sockets.in(`uid_${uid}`).emit('event:chats.receive', data);
-        });
-        if (messageObj.system) {
-            return;
-        }
-        // Delayed notifications
-        let queueObj = Messaging.notifyQueue[`${fromUid}:${roomId}`];
-        if (queueObj) {
-            queueObj.message.content += `\n${messageObj.content}`;
-            clearTimeout(queueObj.timeout);
-        }
-        else {
-            queueObj = {
-                message: messageObj,
-            };
-            Messaging.notifyQueue[`${fromUid}:${roomId}`] = queueObj;
-        }
-        queueObj.timeout = setTimeout(() => __awaiter(this, void 0, void 0, function* () {
-            try {
-                yield sendNotifications(fromUid, uids, roomId, queueObj.message);
-            }
-            catch (err) {
-                winston.error(`[messaging/notifications] Unabled to send notification\n${err.stack}`);
-            }
-        }), meta.config.notificationSendDelay * 1000);
-    });
-    function sendNotifications(fromuid, uids, roomId, messageObj) {
+module.exports = function configureMessaging(Messaging) {
+    const notifyQueue = {};
+    function sendNotifications(fromUid, uids, roomId, messageObj) {
         return __awaiter(this, void 0, void 0, function* () {
             const isOnline = yield user.isOnline(uids);
-            uids = uids.filter((uid, index) => !isOnline[index] && parseInt(fromuid, 10) !== parseInt(uid, 10));
+            uids = uids.filter((uid, index) => !isOnline[index] && parseInt(fromUid, 10) !== parseInt(uid, 10));
             if (!uids.length) {
                 return;
             }
             const { displayname } = messageObj.fromUser;
             const isGroupChat = yield Messaging.isGroupChat(roomId);
+            const notificationType = isGroupChat ? 'new-group-chat' : 'new-chat';
             const notification = yield notifications.create({
-                type: isGroupChat ? 'new-group-chat' : 'new-chat',
+                type: notificationType,
                 subject: `[[email:notif.chat.subject, ${displayname}]]`,
                 bodyShort: `[[notifications:new_message_from, ${displayname}]]`,
                 bodyLong: messageObj.content,
-                nid: `chat_${fromuid}_${roomId}`,
-                from: fromuid,
+                nid: `chat_${fromUid}_${roomId}`,
+                from: fromUid,
                 path: `/chats/${messageObj.roomId}`,
             });
-            delete Messaging.notifyQueue[`${fromuid}:${roomId}`];
+            delete notifyQueue[`${fromUid}:${roomId}`];
             notifications.push(notification, uids);
         });
     }
+    function notifyUsersInRoom(fromUid, roomId, messageObj) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let uids = yield Messaging.getUidsInRoom(roomId, 0, -1);
+            uids = yield user.blocks.filterUids(fromUid, uids);
+            let data = {
+                roomId: roomId,
+                fromUid: fromUid,
+                message: messageObj,
+                uids: uids,
+            };
+            data = yield plugins.hooks.fire('filter:messaging.notify', data);
+            if (!data || !data.uids || !data.uids.length) {
+                return;
+            }
+            uids = data.uids;
+            uids.forEach((uid) => {
+                data.self = parseInt(uid, 10) === parseInt(fromUid, 10) ? 1 : 0;
+                Messaging.pushUnreadCount(uid);
+                sockets.in(`uid_${uid}`).emit('event:chats.receive', data);
+            });
+            if (messageObj.system) {
+                return;
+            }
+            let queueObj = notifyQueue[`${fromUid}:${roomId}`];
+            if (queueObj) {
+                queueObj.message.content += `\n${messageObj.content}`;
+                clearTimeout(queueObj.timeout);
+            }
+            else {
+                queueObj = {
+                    roomId: roomId,
+                    uid: fromUid,
+                    message: messageObj,
+                    timeout: setTimeout(() => __awaiter(this, void 0, void 0, function* () {
+                        try {
+                            yield sendNotifications(fromUid, uids, roomId, queueObj.message);
+                        }
+                        catch (err) {
+                            winston.error(`[messaging/notifications] Unable to send notification\n${err.stack}`);
+                        }
+                    }), meta.config.notificationSendDelay * 1000),
+                };
+                notifyQueue[`${fromUid}:${roomId}`] = queueObj;
+            }
+            queueObj.timeout = setTimeout(() => __awaiter(this, void 0, void 0, function* () {
+                try {
+                    yield sendNotifications(fromUid, uids, roomId, queueObj.message);
+                }
+                catch (err) {
+                    winston.error(`[messaging/notifications] Unable to send notification\n${err.stack}`);
+                }
+            }), meta.config.notificationSendDelay * 1000);
+        });
+    }
+    return {
+        notifyUsersInRoom,
+    };
 };
--- src/messaging/notifications.ts
+++ src/messaging/notifications.ts
@@ -1,4 +1,3 @@
-//typescript for the imports
 import * as winston from 'winston';
 import * as user from '../user';
 import * as notifications from '../notifications';
@@ -6,22 +5,80 @@ import * as sockets from '../socket.io';
 import * as plugins from '../plugins';
 import * as meta from '../meta';
 
-//javascript i copied and pasted
-'use strict';
-module.exports = function (Messaging) {
-    Messaging.notifyQueue = {}; // Only used to notify a user of a new chat message, see Messaging.notifyUser
 
-    Messaging.notifyUsersInRoom = async (fromUid, roomId, messageObj) => {
-        let uids = await Messaging.getUidsInRoom(roomId, 0, -1);
+// Define the Message interface
+interface Message {
+    roomId: string,
+    system: boolean;
+    content: string;
+    fromUser: {
+        displayname: string;
+    };
+}
+
+interface Messaging {
+    getUidsInRoom(roomId: string, start: number, end: number): Promise<string[]>;
+    isGroupChat(roomId: string): Promise<boolean>;
+    pushUnreadCount(uid: string): void;
+}
+
+type queueObject = {
+    roomId: string;
+    uid: string;
+    message: Message;
+    timeout: NodeJS.Timeout;
+};
+
+export = function configureMessaging(Messaging: Messaging): { notifyUsersInRoom: typeof notifyUsersInRoom } {
+    const notifyQueue: Record<string, queueObject> = {};
+
+    async function sendNotifications(fromUid: string, uids: string[], roomId: string, messageObj: Message):
+    Promise<void> {
+        const isOnline = await user.isOnline(uids);
+        uids = uids.filter((uid, index) => !isOnline[index] && parseInt(fromUid, 10) !== parseInt(uid, 10));
+
+        if (!uids.length) {
+            return;
+        }
+
+        const { displayname } = messageObj.fromUser;
+
+        const isGroupChat = await Messaging.isGroupChat(roomId);
+        const notificationType = isGroupChat ? 'new-group-chat' : 'new-chat';
+
+        const notification = await notifications.create({
+            type: notificationType,
+            subject: `[[email:notif.chat.subject, ${displayname}]]`,
+            bodyShort: `[[notifications:new_message_from, ${displayname}]]`,
+            bodyLong: messageObj.content,
+            nid: `chat_${fromUid}_${roomId}`,
+            from: fromUid,
+            path: `/chats/${messageObj.roomId}`,
+        });
+
+        delete notifyQueue[`${fromUid}:${roomId}`];
+        notifications.push(notification, uids);
+    }
+
+    async function notifyUsersInRoom(fromUid: string, roomId: string, messageObj: Message): Promise<void> {
+        let uids: string[] = await Messaging.getUidsInRoom(roomId, 0, -1);
         uids = await user.blocks.filterUids(fromUid, uids);
 
-        let data = {
+        let data: {
+            roomId: string;
+            fromUid: string;
+            message: Message;
+            uids: string[];
+            self?: 0 | 1; // Define the type for 'self'
+        } = {
             roomId: roomId,
             fromUid: fromUid,
             message: messageObj,
             uids: uids,
         };
+
         data = await plugins.hooks.fire('filter:messaging.notify', data);
+
         if (!data || !data.uids || !data.uids.length) {
             return;
         }
@@ -32,51 +89,42 @@ module.exports = function (Messaging) {
             Messaging.pushUnreadCount(uid);
             sockets.in(`uid_${uid}`).emit('event:chats.receive', data);
         });
+
         if (messageObj.system) {
             return;
         }
-        // Delayed notifications
-        let queueObj = Messaging.notifyQueue[`${fromUid}:${roomId}`];
+
+        let queueObj = notifyQueue[`${fromUid}:${roomId}`];
         if (queueObj) {
             queueObj.message.content += `\n${messageObj.content}`;
             clearTimeout(queueObj.timeout);
         } else {
             queueObj = {
+                roomId: roomId,
+                uid: fromUid, // You may need to adjust this part based on your application's logic
                 message: messageObj,
+                timeout: setTimeout(async () => {
+                    try {
+                        await sendNotifications(fromUid, uids, roomId, queueObj.message);
+                    } catch (err) {
+                        winston.error(`[messaging/notifications] Unable to send notification\n${err.stack}`);
+                    }
+                }, meta.config.notificationSendDelay * 1000),
             };
-            Messaging.notifyQueue[`${fromUid}:${roomId}`] = queueObj;
+            notifyQueue[`${fromUid}:${roomId}`] = queueObj;
         }
 
         queueObj.timeout = setTimeout(async () => {
             try {
                 await sendNotifications(fromUid, uids, roomId, queueObj.message);
             } catch (err) {
-                winston.error(`[messaging/notifications] Unabled to send notification\n${err.stack}`);
+                winston.error(`[messaging/notifications] Unable to send notification\n${err.stack}`);
             }
         }, meta.config.notificationSendDelay * 1000);
-    };
-
-    async function sendNotifications(fromuid, uids, roomId, messageObj) {
-        const isOnline = await user.isOnline(uids);
-        uids = uids.filter((uid, index) => !isOnline[index] && parseInt(fromuid, 10) !== parseInt(uid, 10));
-        if (!uids.length) {
-            return;
-        }
-
-        const { displayname } = messageObj.fromUser;
-
-        const isGroupChat = await Messaging.isGroupChat(roomId);
-        const notification = await notifications.create({
-            type: isGroupChat ? 'new-group-chat' : 'new-chat',
-            subject: `[[email:notif.chat.subject, ${displayname}]]`,
-            bodyShort: `[[notifications:new_message_from, ${displayname}]]`,
-            bodyLong: messageObj.content,
-            nid: `chat_${fromuid}_${roomId}`,
-            from: fromuid,
-            path: `/chats/${messageObj.roomId}`,
-        });
-
-        delete Messaging.notifyQueue[`${fromuid}:${roomId}`];
-        notifications.push(notification, uids);
     }
-};
+
+    return {
+        notifyUsersInRoom,
+    };
+}
+
